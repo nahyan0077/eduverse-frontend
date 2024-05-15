a1ea718b88df082ce454b36c59f07b8abc753c25
nahyan0077 <nahyanmp4@gmail.com>
2024-05-15 20:24:17 +0530

    commented

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 
 import './App.css'
-import Login from './components/auth/Login'
+// import Login from './components/auth/Login'
 import './index.css'
-import SignUp from './components/auth/SignUp'
+// import SignUp from './components/auth/SignUp'
 import Home from './pages/user/Home'
 
 function App() {
