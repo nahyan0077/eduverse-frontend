318cb8f209a5a60c6eaa32e9c4c713b987fbf868
nahyan0077 <nahyanmp4@gmail.com>
2024-06-01 15:19:23 +0530

    feat: block and unblock feature added

--- src/components/admin/AdminInstructors.tsx
+++ src/components/admin/AdminInstructors.tsx
@@ -2,80 +2,177 @@ import { useAppDispatch } from "@/hooks/hooks";
 import { getAllInstructorsAction } from "@/redux/store/actions/user";
 import { useEffect, useState } from "react";
 import LoadingPopUp from "../common/skeleton/LoadingPopUp";
-import DoneIcon from '@mui/icons-material/Done';
-import CloseIcon from '@mui/icons-material/Close';
-import { format } from 'date-fns';
-
+import DoneIcon from "@mui/icons-material/Done";
+import CloseIcon from "@mui/icons-material/Close";
+import { format } from "date-fns";
+import { blockUserAction } from "@/redux/store/actions/admin";
+import ConfirmModal from "@/components/common/modal/ConfirmModal";
+import { ToastContainer, toast } from "react-toastify";
 
 interface Instructor {
-    id: number;
-    userName: string;
-    createdAt: string;
-    isVerified: boolean;
-    isBlocked: boolean;
+	_id: string;
+	userName: string;
+	createdAt: string;
+	isVerified: boolean;
+	isBlocked: boolean;
 }
 
 export const AdminInstructors: React.FC = () => {
-    const dispatch = useAppDispatch();
-    const [instructors, setInstructors] = useState<Instructor[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
+	const dispatch = useAppDispatch();
+	const [instructors, setInstructors] = useState<Instructor[]>([]);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<string | null>(null);
+	const [isModalVisible, setModalVisible] = useState(false);
+	const [selectedInstructor, setSelectedInstructor] = useState<{
+		id: string;
+		isBlocked: boolean;
+	} | null>(null);
 
-    useEffect(() => {
-        const fetchInstructors = async () => {
-            try {
-                const resultAction = await dispatch(getAllInstructorsAction({ page: 1, limit: 10 }));
-                console.log(resultAction,"aciton result get instr");
+	useEffect(() => {
+		const fetchInstructors = async () => {
+			try {
+				const resultAction = await dispatch(
+					getAllInstructorsAction({ page: 1, limit: 10 })
+				);
+				console.log(resultAction, "aciton result get instr");
 
-                if (getAllInstructorsAction.fulfilled.match(resultAction)) {
-                    setInstructors(resultAction.payload.data);
-                } else {
-                    setError('Failed to fetch instructors');
-                }
-            } catch (err) {
-                setError('An error occurred');
-            } finally {
-                setLoading(false);
-            }
-        };
+				if (getAllInstructorsAction.fulfilled.match(resultAction)) {
+					setInstructors(resultAction.payload.data);
+				} else {
+					setError("Failed to fetch instructors");
+				}
+			} catch (err) {
+				setError("An error occurred");
+			} finally {
+				setLoading(false);
+			}
+		};
 
-        fetchInstructors();
-    }, [dispatch]);
+		fetchInstructors();
+	}, [dispatch]);
 
-    if (loading) {
-        return <LoadingPopUp isLoading={loading} />;
-    }
+	const handleDelete = async () => {
+		if (selectedInstructor) {
+			const response = await dispatch(
+				blockUserAction({
+					id: selectedInstructor.id,
+					isBlocked: !selectedInstructor.isBlocked,
+				})
+			);
+			console.log(response, "block and unblock user..!");
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
+			if (blockUserAction.fulfilled.match(response)) {
+				setInstructors((prevInstructors) =>
+					prevInstructors.map((instructor) =>
+						instructor._id === selectedInstructor.id
+							? { ...instructor, isBlocked: !selectedInstructor.isBlocked }
+							: instructor
+					)
+				);
+				toast.success("User blocked/unblocked successfully", {
+					position: "top-right",
+					autoClose: 4000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "dark",
+				});
+			} else {
+				toast.error("Error occurred", {
+					position: "top-right",
+					autoClose: 4000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "dark",
+				});
+			}
+			setModalVisible(false);
+			setSelectedInstructor(null);
+		}
+	};
 
-    return (
-        <div className="overflow-x-auto max-w-full mx-auto p-8">
-            <h1 className="text-3xl font-bold ml-10 mb-10" >Instructors</h1>
-            <table className="table table-lg">
-                <thead className="text-lg uppercase text-center" >
-                    <tr>
-                        <th>Si.No</th>
-                        <th>Name</th>
-                        <th>Joined</th>
-                        <th>verified</th>
-                        <th>status</th>
-                    </tr>
-                </thead>
-                <tbody className="text-center" >
-                    {instructors.map((instructor, index) => (
-                        <tr key={instructor.id} className="hover:bg-gray-800">
-                            <th>{index + 1}</th>
-                            <td>{instructor.userName}</td>
-                            <td>{ format(new Date(instructor.createdAt), 'dd-MM-yyyy') }</td>
-                            <td>{instructor.isVerified ? <DoneIcon /> : <CloseIcon/> }</td>
-                            <td>{instructor.isBlocked ? <button className="btn btn-sm btn-outline btn-primary">Unblock</button> : 
-                            <button className="btn btn-sm  btn-outline btn-error">Block</button> }</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+	const handleCancel = () => {
+		console.log("Action cancelled");
+		setModalVisible(false);
+	};
+
+	const handleBlock = (instructorId: string, isBlocked: boolean) => {
+		console.log(instructorId, isBlocked, "blk and ukb");
+
+		setSelectedInstructor({ id: instructorId, isBlocked });
+		setModalVisible(true);
+	};
+
+	if (loading) {
+		return <LoadingPopUp isLoading={loading} />;
+	}
+
+	if (error) {
+		return <div>Error: {error}</div>;
+	}
+
+	return (
+		<div className="overflow-x-auto max-w-full mx-auto p-8">
+			<ToastContainer />
+			{isModalVisible && (
+				<ConfirmModal
+					message={` ${
+						selectedInstructor?.isBlocked ? "Unblock" : "Block"
+					}  this instructor  `}
+					onConfirm={handleDelete}
+					onCancel={handleCancel}
+				/>
+			)}
+			<h1 className="text-3xl font-bold ml-10 mb-10">Instructors</h1>
+			<table className="table table-lg">
+				<thead className="text-lg uppercase text-center">
+					<tr>
+						<th>Si.No</th>
+						<th>Name</th>
+						<th>Joined</th>
+						<th>Verified</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				<tbody className="text-center">
+					{instructors.map((instructor, index) => (
+						<tr key={instructor._id} className="hover:bg-gray-800">
+							<th>{index + 1}</th>
+							<td>{instructor.userName}</td>
+							<td>{format(new Date(instructor.createdAt), "dd-MM-yyyy")}</td>
+							<td>{instructor.isVerified ? <DoneIcon /> : <CloseIcon />}</td>
+							<td>
+								{instructor.isBlocked ? (
+									<button
+										className="btn btn-sm btn-outline btn-primary"
+										onClick={() =>
+											handleBlock(instructor._id, instructor.isBlocked)
+										}
+									>
+										Unblock
+									</button>
+								) : (
+									<button
+										className="btn btn-sm btn-outline btn-error"
+										onClick={() =>
+											handleBlock(instructor._id, instructor.isBlocked)
+										}
+									>
+										Block
+									</button>
+								)}
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 };
+
+export default AdminInstructors;
--- src/components/admin/AdminStudents.tsx
+++ src/components/admin/AdminStudents.tsx
@@ -2,119 +2,170 @@ import { useAppDispatch } from "@/hooks/hooks";
 import { getAllStudentsAction } from "@/redux/store/actions/user";
 import { useEffect, useState } from "react";
 import LoadingPopUp from "../common/skeleton/LoadingPopUp";
-import DoneIcon from '@mui/icons-material/Done';
-import CloseIcon from '@mui/icons-material/Close';
+import DoneIcon from "@mui/icons-material/Done";
+import CloseIcon from "@mui/icons-material/Close";
+import { blockUserAction } from "@/redux/store/actions/admin";
+import ConfirmModal from "@/components/common/modal/ConfirmModal";
+import { ToastContainer, toast } from "react-toastify";
 
 interface Student {
-    _id: string;
-    userName: string;
-    profession: string;
-    isVerified: boolean;
-    isBlocked: boolean;
+	_id: string;
+	userName: string;
+	profession: string;
+	isVerified: boolean;
+	isBlocked: boolean;
 }
 
 export const AdminStudents: React.FC = () => {
-    const dispatch = useAppDispatch();
-    const [students, setStudents] = useState<Student[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
-    const [selectedStudentId, setSelectedStudentId] = useState<number | null>(null);
-    const [showConfirmationModal, setShowConfirmationModal] = useState<boolean>(false);
+	const dispatch = useAppDispatch();
+	const [students, setStudents] = useState<Student[]>([]);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<string | null>(null);
+	const [isModalVisible, setModalVisible] = useState(false);
+	const [selectedStudent, setSelectedStudent] = useState<{
+		id: string;
+		isBlocked: boolean;
+	} | null>(null);
 
-    useEffect(() => {
-        const fetchStudents = async () => {
-            try {
-                const resultAction = await dispatch(getAllStudentsAction({ page: 1, limit: 10 }));
-                console.log(resultAction,"action result get stuu");
+	useEffect(() => {
+		const fetchStudents = async () => {
+			try {
+				const resultAction = await dispatch(
+					getAllStudentsAction({ page: 1, limit: 10 })
+				);
+				console.log(resultAction, "action result get stuu");
 
-                if (getAllStudentsAction.fulfilled.match(resultAction)) {
-                    setStudents(resultAction.payload.data);
-                } else {
-                    setError('Failed to fetch students');
-                }
-            } catch (err) {
-                setError('An error occurred');
-            } finally {
-                setLoading(false);
-            }
-        };
+				if (getAllStudentsAction.fulfilled.match(resultAction)) {
+					setStudents(resultAction.payload.data);
+				} else {
+					setError("Failed to fetch students");
+				}
+			} catch (err) {
+				setError("An error occurred");
+			} finally {
+				setLoading(false);
+			}
+		};
 
-        fetchStudents();
-    }, [dispatch]);
+		fetchStudents();
+	}, [dispatch]);
 
-    const handleBlock = (studentId: string) => {
-        // setSelectedStudentId(studentId);
-        setShowConfirmationModal(true);
-    };
+	const handleDelete = async () => {
+		if (selectedStudent) {
+			const response = await dispatch(
+				blockUserAction({
+					id: selectedStudent.id,
+					isBlocked: !selectedStudent.isBlocked,
+				})
+			);
+			console.log(response, "block and unblock user..!");
 
-    const handleConfirmBlock = () => {
-        if (selectedStudentId !== null) {
-            // Dispatch blockStudentAction with the selectedStudentId
-            // dispatch(blockStudentAction(selectedStudentId));
-            // Close the modal
-            setShowConfirmationModal(false);
-        }
-    };
+			if (blockUserAction.fulfilled.match(response)) {
+				setStudents((prevStudents) =>
+					prevStudents.map((student) =>
+						student._id === selectedStudent.id
+							? { ...student, isBlocked: !selectedStudent.isBlocked }
+							: student
+					)
+				);
+				toast.success("User blocked/unblocked successfully", {
+					position: "top-right",
+					autoClose: 4000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "dark",
+				});
+			} else {
+				toast.error("Error occurred", {
+					position: "top-right",
+					autoClose: 4000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "dark",
+				});
+			}
+			setModalVisible(false);
+			setSelectedStudent(null);
+		}
+	};
 
-    const handleCancelBlock = () => {
-        // Clear the selected student id and close the modal
-        setSelectedStudentId(null);
-        setShowConfirmationModal(false);
-    };
+	const handleCancel = () => {
+		console.log("Action cancelled");
+		setModalVisible(false);
+	};
 
-    if (loading) {
-        return <LoadingPopUp isLoading={loading} />;
-    }
+	const handleBlock = (studentId: string, isBlocked: boolean) => {
+		setSelectedStudent({ id: studentId, isBlocked });
+		setModalVisible(true);
+	};
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
+	if (loading) {
+		return <LoadingPopUp isLoading={loading} />;
+	}
 
-    return (
-        <div className="overflow-x-auto max-w-full mx-auto p-8">
-            <h1 className="text-3xl font-bold ml-10 mb-10" >Students</h1>
-            <table className="table table-lg">
-                <thead className="text-lg uppercase text-center" >
-                    <tr>
-                        <th>Si.No</th>
-                        <th>Name</th>
-                        <th>Profession</th>
-                        <th>Verified</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody className="text-center" >
-                    {students.map((student, index) => (
-                        <tr key={student._id} className="hover:bg-gray-800">
-                            <th>{index + 1}</th>
-                            <td>{student.userName}</td>
-                            <td>{student.profession}</td>
-                            <td>{student.isVerified ? <DoneIcon /> : <CloseIcon/> }</td>
-                            <td>
-                                {student.isBlocked ? 
-                                    <button className="btn btn-sm btn-outline btn-primary" onClick={() => handleBlock(student._id)}>Unblock</button> : 
-                                    <button className="btn btn-sm btn-outline btn-error" onClick={() => handleBlock(student._id)}>Block</button>
-                                }
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+	if (error) {
+		return <div>Error: {error}</div>;
+	}
 
-            {/* Confirmation Modal */}
-            {showConfirmationModal && (
-                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="bg-gray-900 p-8 rounded-lg shadow-lg">
-                        <p>Are you sure you want to block this student?</p>
-                        <div className="flex justify-end mt-4">
-                            <button className="btn btn-sm btn-outline btn-primary mr-2" onClick={handleConfirmBlock}>Yes</button>
-                            <button className="btn btn-sm btn-outline btn-error" onClick={handleCancelBlock}>No</button>
-                        </div>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
+	return (
+		<div className="overflow-x-auto max-w-full mx-auto p-8">
+			<ToastContainer />
+			{isModalVisible && (
+				<ConfirmModal
+					message={` ${
+						selectedStudent?.isBlocked ? "Unblock" : "Block"
+					}  this student  `}
+					onConfirm={handleDelete}
+					onCancel={handleCancel}
+				/>
+			)}
+			<h1 className="text-3xl font-bold ml-10 mb-10">Students</h1>
+			<table className="table table-lg">
+				<thead className="text-lg uppercase text-center">
+					<tr>
+						<th>Si.No</th>
+						<th>Name</th>
+						<th>Profession</th>
+						<th>Verified</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				<tbody className="text-center">
+					{students.map((student, index) => (
+						<tr key={student._id} className="hover:bg-gray-800">
+							<th>{index + 1}</th>
+							<td>{student.userName}</td>
+							<td>{student.profession}</td>
+							<td>{student.isVerified ? <DoneIcon /> : <CloseIcon />}</td>
+							<td>
+								{student.isBlocked ? (
+									<button
+										className="btn btn-sm btn-outline btn-primary"
+										onClick={() => handleBlock(student._id, student.isBlocked)}
+									>
+										Unblock
+									</button>
+								) : (
+									<button
+										className="btn btn-sm btn-outline btn-error"
+										onClick={() => handleBlock(student._id, student.isBlocked)}
+									>
+										Block
+									</button>
+								)}
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default AdminStudents;
