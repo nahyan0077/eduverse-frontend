234663628d1d2e9f9d9179e4ea1bd4038f569c7b
nahyan0077 <nahyanmp4@gmail.com>
2024-06-19 21:50:32 +0530

    feat: enrolled students diplayed

--- src/components/student/StudentEnrollments.tsx
+++ src/components/student/StudentEnrollments.tsx
@@ -1,9 +1,45 @@
-import React from "react";
+import { useAppDispatch } from "@/hooks/hooks";
+import { RootState } from "@/redux/store";
+import { getEnrollmentByUserIdAction } from "@/redux/store/actions/enrollment";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-export const StudentEnrollment : React.FC = () => {
-    return(
-        <>
-            
-        </>
-    )
-}
+export const StudentEnrollment: React.FC = () => {
+    const { data } = useSelector((state: RootState) => state.user);
+    const dispatch = useAppDispatch();
+    const [enrollments, setEnrollments] = useState<any[]>([]);
+
+    useEffect(() => {
+        if (data && data._id) {
+            fetchEnrollments();
+        }
+    }, [data]);
+
+    const fetchEnrollments = async () => {
+        if (data && data._id) {
+            const response: any = await dispatch(getEnrollmentByUserIdAction(data._id));
+            setEnrollments(response.payload.data);
+        }
+    };
+
+    return (
+        <div className="max-w-full mx-auto p-28 flex gap-20">
+            {enrollments.map((enrollment: any) => (
+                <div key={enrollment._id} className="card w-96 glass">
+                    <figure>
+                        <img
+                            src={enrollment.courseId.thumbnail}
+                            alt={enrollment.courseId.title}
+                        />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{enrollment.courseId.title}</h2>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary btn-sm">Learn now!</button>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
