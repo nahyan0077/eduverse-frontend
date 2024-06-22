6c071e7002125a4c6d04e2d17156aeb140956c85
nahyan0077 <nahyanmp4@gmail.com>
2024-06-22 20:32:02 +0530

    feat: global search feature

--- src/redux/store/actions/course/searchCourseAction.ts
+++ src/redux/store/actions/course/searchCourseAction.ts
@@ -6,7 +6,14 @@ import { AxiosError } from "axios";
 export const searchCourseAction = createAsyncThunk (
     "course/searchCourse" , async (query: string, {rejectWithValue}) => {
         try {
-            const response = await CLIENT_API.get(`/api/course/search?${query}`,config)
+			
+			console.log(query,"heee");
+			
+
+            const response = await CLIENT_API.get(`/api/course/search?query=${encodeURIComponent(query)}`,config)
+
+			console.log(response,"helooo");
+			
 
 			if (response.data.success) {
 				return response.data;
