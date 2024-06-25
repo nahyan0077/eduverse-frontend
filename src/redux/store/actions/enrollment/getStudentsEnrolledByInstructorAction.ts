1d800d8770cc42ccf4fa82fc9cafda63b2d841d2
nahyan0077 <nahyanmp4@gmail.com>
2024-06-25 12:30:40 +0530

    feat: get students enrolled under an instructor action setup

---
+++ src/redux/store/actions/enrollment/getStudentsEnrolledByInstructorAction.ts
@@ -0,0 +1,28 @@
+import { config } from "@/common/configurations";
+import { CLIENT_API } from "@/utils/axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { AxiosError } from "axios";
+
+export const getStudentsEnrolledByInstructor = createAsyncThunk(
+	"course/getStudentsEnrolledByInstructor",
+	async (instructorId: string, { rejectWithValue }) => {
+		try {
+			const response = await CLIENT_API.get(
+				`/api/course/enrollment/student/${instructorId}`,
+				config
+			);
+			if (response.data.success) {
+				return response.data;
+			} else {
+				return rejectWithValue(response.data);
+			}
+		} catch (error) {
+			console.log(
+				"get students enrolled under an instructor action Error: ",
+				error
+			);
+			const e: AxiosError = error as AxiosError;
+			return rejectWithValue(e.response?.data || e.message);
+		}
+	}
+);
