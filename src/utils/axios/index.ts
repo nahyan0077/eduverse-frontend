ba70bee21b73c2135e5e174f843612581c3c81c7
nahyan0077 <nahyanmp4@gmail.com>
2024-07-15 10:12:32 +0530

    feat: updated axios base URL

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-export const BASE_URL = "http://localhost:2001"
+export const BASE_URL = "http://drop-ship.shop"
+// export const BASE_URL = "http://localhost:2001"
 
 export const CLIENT_API = axios.create({
     baseURL : BASE_URL
