3b23cfa4c1353e739d053ce5be937faa66751e39
nahyan0077 <nahyanmp4@gmail.com>
2024-08-17 15:15:34 +0530

    updated links for hosting

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // export const BASE_URL = "https://drop-ship.shop"
-export const BASE_URL = "http://localhost:2001"
-// export const BASE_URL = "https://eduverse-api.onrender.com"
+// export const BASE_URL = "http://localhost:2001"
+export const BASE_URL = "https://eduverse-api.onrender.com"
 
 export const CLIENT_API = axios.create({
     baseURL : BASE_URL
