151d1c78b3e55dbfdff9a64f69c813a5f702fac8
nahyan0077 <nahyanmp4@gmail.com>
2024-08-23 12:23:28 +0530

    added vecel json

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,9 +1,24 @@
+// import axios from 'axios'
+
+// // export const BASE_URL = "https://drop-ship.shop"
+// // export const BASE_URL = "http://localhost:2001"
+// export const BASE_URL = "https://eduverse-api.onrender.com"
+
+// export const CLIENT_API = axios.create({
+//     baseURL : BASE_URL
+// })
+
 import axios from 'axios'
 
-// export const BASE_URL = "https://drop-ship.shop"
-// export const BASE_URL = "http://localhost:2001"
-export const BASE_URL = "https://eduverse-api.onrender.com"
+const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+
+export const BASE_URL = isDevelopment 
+  ? "http://localhost:2001"
+  : "https://eduverse-api.onrender.com"
+
+  console.log(BASE_URL,"hee");
+  
 
 export const CLIENT_API = axios.create({
-    baseURL : BASE_URL
+    baseURL: BASE_URL
 })
